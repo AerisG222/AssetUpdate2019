7a568c423f7123770a6d834442dc01f996279e27
Mike Morano <mmorano@mikeandwan.us>
2019-02-18 18:43:16 -0500

    start to pull details from files on disk

---
+++ src/AssetUpdate2019/Data/Storage.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using NMagickWand;
+
+
+namespace AssetUpdate2019.Data
+{
+    class Storage
+    {
+        readonly object _lockObj = new object();
+        readonly ParallelOptions _parallelOpts;
+        readonly string _photoRoot;
+        readonly string _videoRoot;
+        readonly List<Media> _photoList = new List<Media>();
+        readonly List<Media> _videoList = new List<Media>();
+
+
+        public Storage(string photoRoot, string videoRoot)
+        {
+            if(string.IsNullOrWhiteSpace(photoRoot))
+            {
+                throw new ArgumentNullException(nameof(photoRoot));
+            }
+
+            if(string.IsNullOrWhiteSpace(videoRoot))
+            {
+                throw new ArgumentNullException(nameof(videoRoot));
+            }
+
+            _photoRoot = photoRoot;
+            _videoRoot = videoRoot;
+
+            int vpus = Math.Max(Environment.ProcessorCount - 1, 1);
+
+            _parallelOpts = new ParallelOptions { MaxDegreeOfParallelism = vpus };
+        }
+
+
+        public IEnumerable<Photo> GetPhotos()
+        {
+            Console.WriteLine("Getting photos");
+
+            GetPhotoMedia();
+
+            Console.WriteLine();
+            Console.WriteLine($"Finished.  Found {_photoList.Count} files");
+
+            return null;
+        }
+
+
+        public IEnumerable<Video> GetVideos()
+        {
+            Console.WriteLine("Getting videos");
+
+            GetVideoMedia();
+
+            Console.WriteLine();
+            Console.WriteLine($"Finished.  Found {_videoList.Count} files");
+
+            return null;
+        }
+
+
+        void GetPhotoMedia()
+        {
+            var files = GetFiles(_photoRoot);
+
+            Parallel.ForEach(files, _parallelOpts, PopulatePhotoMedia);
+        }
+
+
+        void GetVideoMedia()
+        {
+            var files = GetFiles(_videoRoot);
+
+            Parallel.ForEach(files, _parallelOpts, PopulateVideoMedia);
+        }
+
+
+        void PopulatePhotoMedia(string path)
+        {
+            var media = BuildMedia(path);
+
+            PopulateImageProperties(path, media);
+
+            lock(_lockObj)
+            {
+                _photoList.Add(media);
+
+                if(_photoList.Count % 2000 == 0)
+                {
+                    Console.Write('.');
+                }
+            }
+        }
+
+
+        void PopulateVideoMedia(string path)
+        {
+            var media = BuildMedia(path);
+
+            if(path.EndsWith("jpg", StringComparison.OrdinalIgnoreCase))
+            {
+                PopulateImageProperties(path, media);
+            }
+            else
+            {
+                PopulateVideoProperties(path, media);
+            }
+
+            lock(_lockObj)
+            {
+                _videoList.Add(media);
+
+                if(_photoList.Count % 100 == 0)
+                {
+                    Console.Write('.');
+                }
+            }
+        }
+
+
+        void PopulateImageProperties(string file, Media media)
+        {
+            using(var wand = new MagickWand(media.Path))
+            {
+                media.Height = (int) wand.ImageHeight;
+                media.Width = (int) wand.ImageWidth;
+            }
+        }
+
+
+        void PopulateVideoProperties(string file, Media media)
+        {
+
+        }
+
+
+        Media BuildMedia(string file)
+        {
+            var fi = new FileInfo(file);
+
+            var media = new Media {
+                Path = file,
+                Size = fi.Length
+            };
+
+            return media;
+        }
+
+
+        IEnumerable<string> GetFiles(string path)
+        {
+            return Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories);
+        }
+    }
+}
--- src/AssetUpdate2019/Program.cs
+++ src/AssetUpdate2019/Program.cs
@@ -9,19 +9,21 @@ namespace AssetUpdate2019
     class Program
     {
         Repository _repo;
+        Storage _storage;
 
 
-        Program(string connString)
+        Program(string connString, string photoRoot, string videoRoot)
         {
             _repo = new Repository(connString);
+            _storage = new Storage(photoRoot, videoRoot);
         }
 
 
         static async Task Main(string[] args)
         {
-            if(args.Length > 0)
+            if(args.Length == 3)
             {
-                var p = new Program(args[0]);
+                var p = new Program(args[0], args[1], args[2]);
 
                 await p.ExecuteAsync();
             }
@@ -39,8 +41,11 @@ namespace AssetUpdate2019
             var photos = await _repo.GetPhotosAsync();
             var videos = await _repo.GetVideosAsync();
 
-            Console.WriteLine($"Found {photos.Count()} photos");
-            Console.WriteLine($"Found {videos.Count()} videos");
+            Console.WriteLine($"Found {photos.Count()} photos in db");
+            Console.WriteLine($"Found {videos.Count()} videos in db");
+
+            var photoFiles = _storage.GetPhotos();
+            var videoFiles = _storage.GetVideos();
 
             MagickWandEnvironment.Terminus();
         }
